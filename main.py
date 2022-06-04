f34fdedc87281e55c2c90fd9f8a95ddd78f005e5
cave42 <the.carlos.ave@gmail.com>
2022-06-03 20:46:22 -0700

    Update

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import pandas as pd
 
-path1 =
+nCovBed = "C:/Users/cave42/PycharmProjects/bedToMaster/Files/nCoV-2019.bed"
+
+df = pd.DataFrame(columns=	['target_start', 'target_end',	'amplicon_name', 'fprimer_start'	'fprimer_end',	'fprimer_name',	'rprimer_start',	'rprimer_end',	'rprimer_name',	'forward_sequence',	'reverse_sequence'])
+
